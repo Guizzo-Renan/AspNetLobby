fff384906cecce4953edcabcb7939e2ea55b60fa
Renan <renanguizzo@gmail.com>
2024-03-01 23:35:42 -0300

    commit v18

--- Program.cs
+++ Program.cs
@@ -37,13 +37,12 @@ builder.WebHost.UseUrls($"http://*:{port}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+ //   app.UseSwagger();
+ //   app.UseSwaggerUI();
+
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
